f23edb6ea793cc02e58a58c68a473c343e4130b6
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-02-05 20:56:11 +0300

    Algorithms - sorts: Selection sort

---
+++ algo/sorts/selection/selection.go
@@ -0,0 +1,19 @@
+package selection
+
+import "github.com/yvv4git/go-algorithms/algo/sorts"
+
+// SelectSort - used as selection sort
+func SelectSort(ar []int) {
+	for i := 0; i < len(ar)-1; i++ {
+		min := i
+		for j := i + 1; j < len(ar); j++ {
+			if ar[min] > ar[j] {
+				min = j
+			}
+		}
+
+		if min != i {
+			sorts.Swap(ar, i, min)
+		}
+	}
+}
---
+++ algo/sorts/selection/selection_test.go
@@ -0,0 +1,36 @@
+package selection
+
+import "testing"
+
+func TestSelectSort(t *testing.T) {
+	type args struct {
+		ar []int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "CASE-1",
+			args: args{
+				ar: []int{2, 1},
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "CASE-2",
+			args: args{
+				ar: []int{2, 1, 4, 3, 6, 5},
+			},
+			want: []int{1, 2, 3, 4, 5, 6},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SelectSort(tt.args.ar)
+		})
+	}
+}
