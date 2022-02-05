21d3518b99c2781e1e85900e3fb978aae95d3e28
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-02-05 20:49:32 +0300

    Algorithms - sorts: Bubble sort

--- algo/sorts/bubble/bubble.go
+++ algo/sorts/bubble/bubble.go
@@ -12,3 +12,25 @@ func BubbleSort(ar []int) {
 		}
 	}
 }
+
+// BubbleSort2 - вариант, где мы сравниваем соседние пары и гоним пузырек наверх.
+func BubbleSort2(ar []int) {
+	for i := 0; i < len(ar); i++ {
+		for j := len(ar) - 1; j > i; j-- {
+			if ar[j-1] > ar[j] {
+				sorts.Swap(ar, j-1, j)
+			}
+		}
+	}
+}
+
+// BubbleSort3 - вариант с тонущим пузырьком (sinking sort), когда тяжелый пузырек опускается вниз.
+func BubbleSort3(ar []int) {
+	for i := 0; i < len(ar); i++ {
+		for j := 1; j < len(ar)-i; j++ {
+			if ar[j-1] > ar[j] {
+				sorts.Swap(ar, j-1, j)
+			}
+		}
+	}
+}
