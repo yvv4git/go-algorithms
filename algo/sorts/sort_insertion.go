d6c1965b42e571b018c4b4822c08f1b58b2fb05f
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-02-05 17:04:07 +0300

    Sorts - insertion sort

---
+++ algo/sorts/sort_insertion.go
@@ -0,0 +1,14 @@
+package sorts
+
+// InsertionSort - used as sort with insertion.
+// Have quadratic complexity - O(n^2).
+func InsertionSort(ar []int) {
+	for i := 1; i < len(ar); i++ {
+		x := ar[i]
+		j := i
+		for ; j >= 1 && ar[j-1] > x; j-- {
+			ar[j] = ar[j-1]
+		}
+		ar[j] = x
+	}
+}
