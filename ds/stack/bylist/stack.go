525eb5ea46e068372561b2559b2069ebaa6d6870
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-02-08 14:31:49 +0300

    Data structures - stack.

---
+++ ds/stack/bylist/stack.go
@@ -0,0 +1,58 @@
+package bylist
+
+import (
+	"container/list"
+	"errors"
+)
+
+const (
+	error_empty           = "Stack is empty"
+	error_type_not_string = "Stack is empty"
+)
+
+// listStack - реализация стека на основе списка.
+type listStack struct {
+	stack *list.List
+}
+
+// Push - т.е. добавить элемент в стек.
+func (s *listStack) Push(value string) {
+	s.stack.PushFront(value)
+}
+
+// Pop - достать элемент.
+func (s *listStack) Pop() (result string, err error) {
+	if s.stack.Len() > 0 {
+		element := s.stack.Front()
+		value, ok := element.Value.(string)
+		if !ok {
+			return "", errors.New(error_type_not_string)
+		}
+		s.stack.Remove(element)
+		return value, nil
+	}
+
+	return "", errors.New(error_empty)
+}
+
+// Front - можно посмотреть значение первого элемента.
+func (s *listStack) Front() (result string, err error) {
+	if s.stack.Len() > 0 {
+		if value, ok := s.stack.Front().Value.(string); ok {
+			return value, nil
+		}
+		return "", errors.New(error_type_not_string)
+	}
+
+	return "", errors.New(error_empty)
+}
+
+// Size - возвращает размер стека.
+func (s *listStack) Size() int {
+	return s.stack.Len()
+}
+
+// Empty - определяет, пустой ли стек.
+func (s *listStack) Empty() bool {
+	return s.stack.Len() == 0
+}
---
+++ ds/stack/bylist/stack_test.go
@@ -0,0 +1,245 @@
+package bylist
+
+import (
+	"container/list"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_listStack_Push(t *testing.T) {
+	type fields struct {
+		stack *list.List
+	}
+	type args struct {
+		value string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "case-1",
+			fields: fields{
+				stack: list.New(),
+			},
+			args: args{
+				value: "Vladimir",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &listStack{
+				stack: tt.fields.stack,
+			}
+			s.Push(tt.args.value)
+
+			value, err := s.Front()
+			assert.Empty(t, err, "Error must be nil")
+			assert.Equal(t, tt.args.value, value, "Values must be equal")
+		})
+	}
+}
+
+func Test_listStack_Pop(t *testing.T) {
+	testList := list.New()
+	testList.PushFront("Vladimir")
+	testList.PushFront("Oly")
+	testList.PushFront("Uly")
+
+	type fields struct {
+		stack *list.List
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		wantResult string
+		wantErr    bool
+	}{
+		{
+			name: "case-1",
+			fields: fields{
+				stack: testList,
+			},
+			wantResult: "Uly",
+			wantErr:    false,
+		},
+		{
+			name: "case-2",
+			fields: fields{
+				stack: testList,
+			},
+			wantResult: "Oly",
+			wantErr:    false,
+		},
+		{
+			name: "case-3",
+			fields: fields{
+				stack: testList,
+			},
+			wantResult: "Vladimir",
+			wantErr:    false,
+		},
+		{
+			name: "case-4",
+			fields: fields{
+				stack: testList,
+			},
+			wantResult: "",
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &listStack{
+				stack: tt.fields.stack,
+			}
+			gotResult, err := s.Pop()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("listStack.Pop() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotResult != tt.wantResult {
+				t.Errorf("listStack.Pop() = %v, want %v", gotResult, tt.wantResult)
+			}
+		})
+	}
+}
+
+func Test_listStack_Front(t *testing.T) {
+	listForTest := list.New()
+	listForTest.PushFront("Vladimir")
+
+	type fields struct {
+		stack *list.List
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		wantResult string
+		wantErr    bool
+	}{
+		{
+			name: "case-1",
+			fields: fields{
+				stack: listForTest,
+			},
+			wantResult: "Vladimir",
+			wantErr:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &listStack{
+				stack: tt.fields.stack,
+			}
+			gotResult, err := s.Front()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("listStack.Front() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotResult != tt.wantResult {
+				t.Errorf("listStack.Front() = %v, want %v", gotResult, tt.wantResult)
+			}
+		})
+	}
+}
+
+func Test_listStack_Size(t *testing.T) {
+	testList := list.New()
+	testList.PushFront("Vladimir")
+
+	testList2 := list.New()
+	testList2.PushFront("Vladimir")
+	testList2.PushFront("Oly")
+
+	testList3 := list.New()
+	testList3.PushFront("Vladimir")
+	testList3.PushFront("Oly")
+	testList3.PushFront("Uly")
+
+	type fields struct {
+		stack *list.List
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{
+			name: "case-1",
+			fields: fields{
+				stack: testList,
+			},
+			want: 1,
+		},
+		{
+			name: "case-2",
+			fields: fields{
+				stack: testList2,
+			},
+			want: 2,
+		},
+		{
+			name: "case-3",
+			fields: fields{
+				stack: testList3,
+			},
+			want: 3,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &listStack{
+				stack: tt.fields.stack,
+			}
+			if got := s.Size(); got != tt.want {
+				t.Errorf("listStack.Size() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_listStack_Empty(t *testing.T) {
+	testList := list.New()
+	testList.PushFront("Vladimir")
+
+	type fields struct {
+		stack *list.List
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "case-1",
+			fields: fields{
+				stack: list.New(),
+			},
+			want: true,
+		},
+		{
+			name: "case-2",
+			fields: fields{
+				stack: testList,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &listStack{
+				stack: tt.fields.stack,
+			}
+			if got := s.Empty(); got != tt.want {
+				t.Errorf("listStack.Empty() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
