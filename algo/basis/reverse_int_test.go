ff55ae09dedc96d08f7883cd24ec6f7b361d40e0
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-03-07 22:16:13 +0300

    Explore reverse int algorithm

--- algo/basis/reverse_int_test.go
+++ algo/basis/reverse_int_test.go
@@ -1,6 +1,8 @@
 package basis
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_reverseInt(t *testing.T) {
 	type args struct {
@@ -59,6 +61,36 @@ func Test_reverseInt(t *testing.T) {
 			if got := reverseInt(tt.args.x); got != tt.want {
 				t.Errorf("reverseInt() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestExploreReverse(t *testing.T) {
+	tests := []struct {
+		name string
+		x    int
+		want int
+	}{
+		{
+			name: "CASE-1",
+			x:    1,
+			want: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			x := tt.x   // число, которое надо развернуть
+			result := 0 // здесь будет результат
+
+			for x > 0 {
+				result = result*10 + x%10
+				t.Logf("%d*10 + %d%%10", x, x)
+				t.Logf("%d + %d", x/10, x%10)
+				x = x / 10
+			}
+
+			t.Logf("Result: %d", result)
 		})
 	}
 }
