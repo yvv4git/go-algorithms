a280b1c6355499b9a6d7519f3ec46d6281f82699
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-03-08 13:14:49 +0300

    Longest common prefix

---
+++ algo/basis/longest_common_prefix_test.go
@@ -0,0 +1,45 @@
+package basis
+
+import "testing"
+
+func Test_longestCommonPrefix(t *testing.T) {
+	type args struct {
+		strs []string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "CASE-1",
+			args: args{
+				strs: []string{"leetcode", "leeter", "lee"},
+			},
+			want: "lee",
+		},
+		{
+			name: "CASE-2",
+			args: args{
+				strs: []string{"flower", "flow", "flight"},
+			},
+			want: "fl",
+		},
+		{
+			name: "CASE-3",
+			args: args{
+				strs: []string{"dog", "racecar", "car"},
+			},
+			want: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestCommonPrefix(tt.args.strs); got != tt.want {
+				t.Errorf("longestCommonPrefix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
