9676d7080d97ad528525c6ec681e4b862586678a
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-03-08 14:11:50 +0300

    Remove element (int)

---
+++ algo/basis/remove_element_int.go
@@ -0,0 +1,22 @@
+package basis
+
+func removeElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	i, j := 0, 1
+	for i < len(nums) {
+		if nums[i] == val {
+			for j < len(nums) && nums[j] == val {
+				j++
+			}
+			if j >= len(nums) {
+				return i
+			}
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+		i++
+		j++
+	}
+	return i
+}
