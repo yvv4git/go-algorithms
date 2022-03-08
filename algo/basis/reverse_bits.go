7951e6cf306f33d3fdd3e302d29c9c9f5c40a44d
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-03-08 13:27:47 +0300

    Reverse bits

---
+++ algo/basis/reverse_bits.go
@@ -0,0 +1,12 @@
+package basis
+
+func reverseBits(num uint32) uint32 {
+	ret := uint32(0)
+	power := uint32(31)
+	for num != 0 {
+		ret += (num & 1) << power
+		num = num >> 1
+		power -= 1
+	}
+	return ret
+}
---
+++ algo/basis/reverse_bits_test.go
@@ -0,0 +1,36 @@
+package basis
+
+import "testing"
+
+func Test_reverseBits(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint32
+	}{
+		{
+			name: "CASE-1",
+			args: args{
+				num: 5,
+			},
+			want: 2684354560,
+		},
+		{
+			name: "CASE-2",
+			args: args{
+				num: 43261596,
+			},
+			want: 964176192,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseBits(tt.args.num); got != tt.want {
+				t.Errorf("reverseBits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
