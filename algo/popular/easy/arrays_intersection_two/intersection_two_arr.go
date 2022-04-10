45dd0afa7488fbf768d79de76174640e6e71b196
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-04-10 15:14:36 +0300

    Intersection two int arrayd - 2

---
+++ algo/popular/easy/arrays_intersection_two/intersection_two_arr.go
@@ -0,0 +1,24 @@
+package arraysintersectiontwo
+
+func intersect(nums1 []int, nums2 []int) []int {
+	index := 0
+	len1 := len(nums1)
+	len2 := len(nums2)
+
+	for i := 0; i < len1; i++ {
+		for j := index; j < len2; j++ {
+
+			// Здесь получается, что в начало первого слайса прописывают совпадающие значения из второго.
+			if nums1[i] == nums2[j] {
+				nums1[index] = nums1[i]
+				nums2[index], nums2[j] = nums2[j], nums2[index]
+				index++
+				break
+			}
+
+		}
+	}
+
+	// На выход отдают только те значения из первого слайса, которые были прописаны.
+	return nums1[:index]
+}
---
+++ algo/popular/easy/arrays_intersection_two/intersection_two_arr_test.go
@@ -0,0 +1,73 @@
+package arraysintersectiontwo
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_intersect(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "CASE-1",
+			args: args{
+				nums1: []int{},
+				nums2: []int{},
+			},
+			want: []int{},
+		},
+		{
+			name: "CASE-2",
+			args: args{
+				nums1: []int{1, 2, 2, 1},
+				nums2: []int{2, 2},
+			},
+			want: []int{2, 2},
+		},
+		{
+			name: "CASE-3",
+			args: args{
+				nums1: []int{4, 9, 5},
+				nums2: []int{9, 4, 9, 8, 4},
+			},
+			want: []int{4, 9},
+		},
+		{
+			name: "CASE-4",
+			args: args{
+				nums1: []int{1, 2, 3},
+				nums2: []int{4, 5, 6},
+			},
+			want: []int{},
+		},
+		{
+			name: "CASE-5",
+			args: args{
+				nums1: []int{1, 2, 3, 4, 5},
+				nums2: []int{4, 5, 6},
+			},
+			want: []int{4, 5},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := intersect(tt.args.nums1, tt.args.nums2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("intersect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExpSlice(t *testing.T) {
+	s := []int{4, 5, 3, 4, 5}
+	t.Logf("%v", s[:2]) // до idx=2 исключительно, т.е. без элемента с idx2
+}
