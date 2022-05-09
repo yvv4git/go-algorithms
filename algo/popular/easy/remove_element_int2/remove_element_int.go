7db384a6cc92081e6e6210fd7e2e3454a3ddb8aa
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-05-09 17:12:30 +0300

    Easy - Remove elemet from int slice and return count elements

---
+++ algo/popular/easy/remove_element_int2/remove_element_int.go
@@ -0,0 +1,14 @@
+package main
+
+func removeElement(nums []int, val int) int {
+	count := 0
+
+	for i, num := range nums {
+		nums[i-count] = nums[i]
+		if num == val {
+			count += 1
+		}
+	}
+
+	return len(nums) - count
+}
