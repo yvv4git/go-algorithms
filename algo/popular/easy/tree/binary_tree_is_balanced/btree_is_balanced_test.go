723202ec957c54bf19aa1ce6cb7e38af390145df
Vladimir Eliseev <vladimir.eliseev@karuna.group>
2022-05-22 17:59:02 +0300

    Easy - Binary tree: Add tests and readme

--- algo/popular/easy/tree/binary_tree_is_balanced/btree_is_balanced_test.go
+++ algo/popular/easy/tree/binary_tree_is_balanced/btree_is_balanced_test.go
@@ -12,7 +12,61 @@ func Test_isBalanced(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "CASE-1",
+			args: args{
+				root: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 9,
+					},
+					Right: &TreeNode{
+						Val: 20,
+						Left: &TreeNode{
+							Val: 15,
+						},
+						Right: &TreeNode{
+							Val: 7,
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "CASE-2",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 3,
+							Left: &TreeNode{
+								Val: 4,
+							},
+							Right: &TreeNode{
+								Val: 4,
+							},
+						},
+						Right: &TreeNode{
+							Val: 3,
+						},
+					},
+					Right: &TreeNode{
+						Val: 2,
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "CASE-3",
+			args: args{
+				root: nil,
+			},
+			want: true,
+		},
 	}
 
 	for _, tt := range tests {
