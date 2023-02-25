a075491d56335fb46b9336c39e03835aee3c20a8
v.eliseev <v.eliseev@iva.ru>
2023-02-25 14:39:33 +0300

    Algorithm popular - string decode

---
+++ algo/popular/medium/strings/decode_string/decode_string_v1.go
@@ -0,0 +1,33 @@
+package decode_string
+
+type item struct {
+	n     int
+	bytes []byte
+}
+
+func decodeStringV1(str string) string {
+	num := 0
+	st := []item{{1, []byte{}}}
+
+	for i := range str {
+		switch {
+		case str[i] == '0':
+			num *= 10
+		case str[i] > '0' && str[i] <= '9':
+			num = num*10 + int(str[i]-'0')
+		case str[i] == '[':
+			st = append(st, item{num, []byte{}})
+			num = 0
+		case str[i] == ']':
+			tmp := st[len(st)-1]
+			st = st[:len(st)-1]
+			for j := 0; j < tmp.n; j++ {
+				st[len(st)-1].bytes = append(st[len(st)-1].bytes, tmp.bytes...)
+			}
+		default:
+			st[len(st)-1].bytes = append(st[len(st)-1].bytes, str[i])
+		}
+	}
+
+	return string(st[0].bytes)
+}
