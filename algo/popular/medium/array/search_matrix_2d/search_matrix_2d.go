c729caf680775874d0b676f872edd186db8c8d9a
yvv4git <yvv4m@mail.ru>
2023-05-10 19:36:54 +0300

    Refactoring

--- algo/popular/medium/array/search_matrix_2d/search_matrix_2d.go
+++ algo/popular/medium/array/search_matrix_2d/search_matrix_2d.go
@@ -1,7 +1,5 @@
 package search_matrix_2d
 
-import "fmt"
-
 func searchMatrix(matrix [][]int, target int) bool {
 	/*
 		Method: Binary search.
@@ -9,14 +7,10 @@ func searchMatrix(matrix [][]int, target int) bool {
 		Time complexity : O(log(n*m))
 		Space complexity : O(1)
 	*/
-	fmt.Printf("=>matrix=%#v \n\n", matrix)
-
 	left, right := 0, len(matrix)*len(matrix[0])
 	for left < right {
 		middle := (left + right) / 2
-		fmt.Printf("=>left=%d right=%d middle=%d \n", left, right, middle)
 		i, j := middle/len(matrix[0]), middle%len(matrix[0]) // Способ найти средний под массив(i), средний элемент в под массиве(j).
-		fmt.Printf("=>i=%d, j=%d \n", i, j)
 
 		if target < matrix[i][j] {
 			right = middle
