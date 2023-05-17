3c8a2c7357e3e39de78b74b6ad90d7ac1610c6ed
yvv4git <yvv4m@mail.ru>
2023-05-17 19:57:56 +0300

    Algorithm - numbers. FizzBuzz

---
+++ algo/popular/easy/number/fizz_buzz/fizz_buzz_v1_switch.go
@@ -0,0 +1,22 @@
+package fizz_buzz
+
+import "strconv"
+
+func fizzBuzzV1(n int) []string {
+	result := []string{}
+
+	for i := 1; i <= n; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			result = append(result, "FizzBuzz")
+		case i%3 == 0 && i%5 != 0:
+			result = append(result, "Fizz")
+		case i%5 == 0 && i%3 != 0:
+			result = append(result, "Buzz")
+		default:
+			result = append(result, strconv.Itoa(i))
+		}
+	}
+
+	return result
+}
