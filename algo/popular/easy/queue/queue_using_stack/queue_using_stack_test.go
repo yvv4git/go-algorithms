fe65e84b86386973a94fd874f37216210b04be17
yvv4git <yvv4m@mail.ru>
2023-05-21 17:27:09 +0300

    Algorithm - queue. Queue using stack

---
+++ algo/popular/easy/queue/queue_using_stack/queue_using_stack_test.go
@@ -0,0 +1,16 @@
+package queue_using_stack
+
+import "testing"
+
+func TestConstructor(t *testing.T) {
+	obj := Constructor()
+	obj.Push(1)
+	obj.Push(2)
+	obj.Push(3)
+	param2 := obj.Pop()
+	param3 := obj.Peek()
+	param4 := obj.Empty()
+	t.Logf("Param-2: %v", param2)
+	t.Logf("Param-3: %v", param3)
+	t.Logf("Param-4: %v", param4)
+}
