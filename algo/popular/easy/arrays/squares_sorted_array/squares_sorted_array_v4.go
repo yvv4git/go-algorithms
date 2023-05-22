ab7a5fa1f8ec58debf3b51bd4e4b473990b49f3f
yvv4git <yvv4m@mail.ru>
2023-05-22 11:48:20 +0300

    Algorithm - ds array. Squares of sorted array. Add new method.

---
+++ algo/popular/easy/arrays/squares_sorted_array/squares_sorted_array_v4.go
@@ -0,0 +1,27 @@
+package squares_sorted_array
+
+func sortedSquaresV4(nums []int) []int {
+	/*
+		Method: Two pointer
+		Time complexity: O(n)
+		Space complexity: O(n) - используется массив, в который помещается результат
+	*/
+	res := make([]int, len(nums))
+	// Три указателя: left, right, i.
+	// Переменная left - указывает на левую границу исходного массива.
+	// Переменная light - указывает на правую границу исходного массива.
+	// Переменная i - это указатель в конец нового массива, результирующего. Он нужен для добавления нового максимального элемента.
+	left, right, i := 0, len(nums)-1, len(nums)-1
+	for left <= right {
+		if nums[left]*nums[left] > nums[right]*nums[right] {
+			res[i] = nums[left] * nums[left]
+			left++
+		} else {
+			res[i] = nums[right] * nums[right]
+			right--
+		}
+		i--
+	}
+
+	return res
+}
