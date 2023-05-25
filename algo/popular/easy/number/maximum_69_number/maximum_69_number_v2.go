31369d33a6061b16e81d5d09cb4902a1cd154256
yvv4git <yvv4m@mail.ru>
2023-05-25 16:47:50 +0300

    Algorithm - numbers. Maximum 69 number

---
+++ algo/popular/easy/number/maximum_69_number/maximum_69_number_v2.go
@@ -0,0 +1,23 @@
+package maximum_69_number
+
+func maximum69NumberV2(num int) int {
+	/*
+		Method: Math
+		Time complexity: O(1) - because we have not more than 4 operations.
+		Space complexity: O(1) - because there are not additional memory for slices.
+	*/
+	div := 1000 // power of 10
+	n := num
+
+	for div > 0 {
+		if n/div == 6 {
+			num += 3 * div // replace 6 with 9
+			return num
+		} else {
+			n = n % div    // move to next digit
+			div = div / 10 // lower power of 10
+		}
+	}
+
+	return num
+}
