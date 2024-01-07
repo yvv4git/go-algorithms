707435d73f277438bc85e6b0c32c7c94963b239b
yvv4git <yvv4m@mail.ru>
2024-01-07 21:58:53 +0300

    Algorithm - string & palindrome. 2131. Longest Palindrome by Concatenating Two Letter Words  [Medium]

---
+++ algo/popular/medium/strings/2131_longest_palindrome_by_concatenating_two_letter_words/ver2_array_test.go
@@ -0,0 +1,52 @@
+package main
+
+import "testing"
+
+func Test_longestPalindromeV2(t *testing.T) {
+	type args struct {
+		words []string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test Case 1",
+			args: args{
+				words: []string{"ab", "ba", "lc", "cl"},
+			},
+			want: 8,
+		},
+		{
+			name: "Test Case 2",
+			args: args{
+				words: []string{"ab", "ty", "yt", "lc", "cl", "ab"},
+			},
+			want: 8,
+		},
+		{
+			name: "Test Case 4",
+			args: args{
+				words: []string{"ab", "ba", "ab", "ba"},
+			},
+			want: 8,
+		},
+		{
+			name: "Test Case 5",
+			args: args{
+				words: []string{"ab", "cd", "dc", "ba"},
+			},
+			want: 8,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindromeV2(tt.args.words); got != tt.want {
+				t.Errorf("longestPalindromeV2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
