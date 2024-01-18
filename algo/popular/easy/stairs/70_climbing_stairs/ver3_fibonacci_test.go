4d75eda4eaf113f3384fbac0cf779972db41cdbe
yvv4git <yvv4m@mail.ru>
2024-01-18 22:18:44 +0300

    Algorithm - dynamic programming & stairs. 70. Climbing Stairs  [easy]

---
+++ algo/popular/easy/stairs/70_climbing_stairs/ver3_fibonacci_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "testing"
+
+func Test_climbStairsV3(t *testing.T) {
+	type args struct {
+		n int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test Case 1: n = 1",
+			args: args{n: 1},
+			want: 1,
+		},
+		{
+			name: "Test Case 2: n = 2",
+			args: args{n: 2},
+			want: 2,
+		},
+		{
+			name: "Test Case 3: n = 3",
+			args: args{n: 3},
+			want: 3,
+		},
+		{
+			name: "Test Case 4: n = 4",
+			args: args{n: 4},
+			want: 5,
+		},
+		{
+			name: "Test Case 5: n = 5",
+			args: args{n: 5},
+			want: 8,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := climbStairsV3(tt.args.n); got != tt.want {
+				t.Errorf("climbStairsV3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
