6c84a3e6d8596cc78a6c816c00a9ffa28bca1d9c
yvv4git <yvv4m@mail.ru>
2024-02-02 22:22:24 +0300

    Algorithms - strings & numbers. 415. Add Strings [Easy]

---
+++ algo/popular/easy/strings/415_add_strings/ver1_math_test.go
@@ -0,0 +1,65 @@
+package main
+
+import "testing"
+
+func Test_addStrings(t *testing.T) {
+	type args struct {
+		num1 string
+		num2 string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Test Case 1: Add two single-digit numbers",
+			args: args{
+				num1: "1",
+				num2: "2",
+			},
+			want: "3",
+		},
+		{
+			name: "Test Case 2: Add two multi-digit numbers",
+			args: args{
+				num1: "123",
+				num2: "456",
+			},
+			want: "579",
+		},
+		{
+			name: "Test Case 3: Add two numbers with different lengths",
+			args: args{
+				num1: "123456789",
+				num2: "987654321",
+			},
+			want: "1111111110",
+		},
+		{
+			name: "Test Case 4: Add two numbers with carry",
+			args: args{
+				num1: "999",
+				num2: "1",
+			},
+			want: "1000",
+		},
+		{
+			name: "Test Case 5: Add two zeros",
+			args: args{
+				num1: "0",
+				num2: "0",
+			},
+			want: "0",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addStrings(tt.args.num1, tt.args.num2); got != tt.want {
+				t.Errorf("addStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
