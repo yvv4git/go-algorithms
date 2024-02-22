9eae3b0d57943275a73a61baa7eb8d7c7aab2350
yvv4git <yvv4m@mail.ru>
2024-02-22 18:36:12 +0300

    Add research in tests

--- algo/popular/medium/bitwise/201_bitwise_and_of_numbers_range/ver1_bitwise_test.go
+++ algo/popular/medium/bitwise/201_bitwise_and_of_numbers_range/ver1_bitwise_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func Test_rangeBitwiseAnd(t *testing.T) {
 	type args struct {
-		m int
-		n int
+		left  int
+		right int
 	}
 
 	tests := []struct {
@@ -13,12 +13,51 @@ func Test_rangeBitwiseAnd(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test Case 1",
+			args: args{
+				left:  5,
+				right: 7,
+			},
+			want: 4,
+		},
+		{
+			name: "Test Case 2",
+			args: args{
+				left:  0,
+				right: 0,
+			},
+			want: 0,
+		},
+		{
+			name: "Test Case 3",
+			args: args{
+				left:  1,
+				right: 2147483647,
+			},
+			want: 0,
+		},
+		{
+			name: "Test Case 4",
+			args: args{
+				left:  10,
+				right: 15,
+			},
+			want: 8,
+		},
+		{
+			name: "Test Case 5",
+			args: args{
+				left:  3,
+				right: 3,
+			},
+			want: 3,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := rangeBitwiseAnd(tt.args.m, tt.args.n); got != tt.want {
+			if got := rangeBitwiseAnd(tt.args.left, tt.args.right); got != tt.want {
 				t.Errorf("rangeBitwiseAnd() = %v, want %v", got, tt.want)
 			}
 		})
--- algo/popular/medium/bitwise/201_bitwise_and_of_numbers_range/ver2_bitwise_test.go
+++ algo/popular/medium/bitwise/201_bitwise_and_of_numbers_range/ver2_bitwise_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_rangeBitwiseAndV2(t *testing.T) {
 	type args struct {
@@ -13,7 +16,46 @@ func Test_rangeBitwiseAndV2(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test Case 1",
+			args: args{
+				left:  5,
+				right: 7,
+			},
+			want: 4,
+		},
+		{
+			name: "Test Case 2",
+			args: args{
+				left:  0,
+				right: 0,
+			},
+			want: 0,
+		},
+		{
+			name: "Test Case 3",
+			args: args{
+				left:  1,
+				right: 2147483647,
+			},
+			want: 0,
+		},
+		{
+			name: "Test Case 4",
+			args: args{
+				left:  10,
+				right: 15,
+			},
+			want: 8,
+		},
+		{
+			name: "Test Case 5",
+			args: args{
+				left:  3,
+				right: 3,
+			},
+			want: 3,
+		},
 	}
 
 	for _, tt := range tests {
@@ -22,5 +64,88 @@ func Test_rangeBitwiseAndV2(t *testing.T) {
 				t.Errorf("rangeBitwiseAndV2() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+// printBinary представляет собой функцию, которая выводит любую переменную в бинарном формате.
+func printBinary(value interface{}) {
+	switch v := value.(type) {
+	case int:
+		fmt.Printf("%032b\n", uint32(v))
+	case int8:
+		fmt.Printf("%08b\n", uint8(v))
+	case int16:
+		fmt.Printf("%016b\n", uint16(v))
+	case int32:
+		fmt.Printf("%032b\n", uint32(v))
+	case int64:
+		fmt.Printf("%064b\n", uint64(v))
+	case uint:
+		fmt.Printf("%032b\n", v)
+	case uint8:
+		fmt.Printf("%08b\n", v)
+	case uint16:
+		fmt.Printf("%016b\n", v)
+	case uint32:
+		fmt.Printf("%032b\n", v)
+	case uint64:
+		fmt.Printf("%064b\n", v)
+	case string:
+		for _, c := range v {
+			fmt.Printf("%08b ", c)
+		}
+		fmt.Println()
+	default:
+		fmt.Println("Unsupported type")
+	}
+}
+
+func TestResearch01(t *testing.T) {
+	left := 3
+	right := 5
+	result := left & right
+	t.Logf("Res: %v", result)
+	printBinary(left)
+	printBinary(right)
+	printBinary(result)
+}
+
+func TestResearch02(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		printBinary(i)
+	}
+}
+
+// Функция для преобразования целого числа в двоичную строку без обрезания ведущих нулей.
+func intToBinaryString(n interface{}) string {
+	switch v := n.(type) {
+	case int:
+		return fmt.Sprintf("%032b", uint32(v))
+	case int8:
+		return fmt.Sprintf("%08b", uint8(v))
+	case int16:
+		return fmt.Sprintf("%016b", uint16(v))
+	case int32:
+		return fmt.Sprintf("%032b", uint32(v))
+	case int64:
+		return fmt.Sprintf("%064b", uint64(v))
+	case uint:
+		return fmt.Sprintf("%032b", v)
+	case uint8:
+		return fmt.Sprintf("%08b", v)
+	case uint16:
+		return fmt.Sprintf("%016b", v)
+	case uint32:
+		return fmt.Sprintf("%032b", v)
+	case uint64:
+		return fmt.Sprintf("%064b", v)
+	default:
+		return "Unsupported type"
+	}
+}
+
+func TestResearch03(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		t.Logf("[%d] => %s", i, intToBinaryString(i))
 	}
 }
