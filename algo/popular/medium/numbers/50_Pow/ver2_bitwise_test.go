7d484ac63d6eaaf5e05ffff2c70bf8f835986412
yvv4git <yvv4m@mail.ru>
2024-02-25 18:01:57 +0300

    Algorithms - numbers. 50. Pow(x, n) [Medium]

---
+++ algo/popular/medium/numbers/50_Pow/ver2_bitwise_test.go
@@ -0,0 +1,65 @@
+package main
+
+import "testing"
+
+func Test_myPowV2(t *testing.T) {
+	type args struct {
+		x float64
+		n int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "Test Case 1: Positive base and positive exponent",
+			args: args{x: 2.0, n: 3},
+			want: 8.0,
+		},
+		{
+			name: "Test Case 2: Positive base and zero exponent",
+			args: args{x: 2.0, n: 0},
+			want: 1.0,
+		},
+		{
+			name: "Test Case 3: Positive base and negative exponent",
+			args: args{x: 2.0, n: -3},
+			want: 0.125,
+		},
+		{
+			name: "Test Case 4: Zero base and positive exponent",
+			args: args{x: 0.0, n: 3},
+			want: 0.0,
+		},
+		{
+			name: "Test Case 5: Zero base and zero exponent",
+			args: args{x: 0.0, n: 0},
+			want: 1.0, // According to mathematical convention 0^0 is 1
+		},
+		{
+			name: "Test Case 6: Negative base and even exponent",
+			args: args{x: -2.0, n: 4},
+			want: 16.0,
+		},
+		{
+			name: "Test Case 7: Negative base and odd exponent",
+			args: args{x: -2.0, n: 3},
+			want: -8.0,
+		},
+		{
+			name: "Test Case 8: Negative base and negative exponent",
+			args: args{x: -2.0, n: -3},
+			want: -0.125,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := myPowV2(tt.args.x, tt.args.n); got != tt.want {
+				t.Errorf("myPowV2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
