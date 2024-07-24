be7a819bd0c882012924196cd71159ccc600d7e9
yvv4git <yvv4m@mail.ru>
2024-07-24 23:56:47 +0300

    Algorithm - 319. Bulb Switcher [Medium]

---
+++ algo/popular/medium/319_bulb_switcher/ver1_sieve_eratosthenes_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "testing"
+
+func Test_bulbSwitch(t *testing.T) {
+	type args struct {
+		n int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test Case 1",
+			args: args{n: 0},
+			want: 0,
+		},
+		{
+			name: "Test Case 2",
+			args: args{n: 1},
+			want: 1,
+		},
+		{
+			name: "Test Case 3",
+			args: args{n: 2},
+			want: 1,
+		},
+		{
+			name: "Test Case 4",
+			args: args{n: 3},
+			want: 1,
+		},
+		{
+			name: "Test Case 5",
+			args: args{n: 4},
+			want: 2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := bulbSwitch(tt.args.n); got != tt.want {
+				t.Errorf("bulbSwitch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
