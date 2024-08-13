64c1040ecfa260272f17b04be8d326071d956c9f
yvv4git <yvv4m@mail.ru>
2024-08-13 21:32:46 +0300

    Algorithm - 368. Largest Divisible Subset [Medium]

---
+++ algo/popular/medium/368_largest_divisible_subset/ver3_dp_memoization.go
@@ -0,0 +1,25 @@
+package main
+
+import "sort"
+
+func largestDivisibleSubsetV3(nums []int) []int {
+	sort.Ints(nums)
+	ans := make([][]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		ans[i] = append(ans[i], nums[i])
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j]%nums[i] == 0 && len(ans[j]) < len(ans[i]) {
+				ans[j] = make([]int, len(ans[i]))
+				copy(ans[j], ans[i])
+			}
+		}
+	}
+
+	var index int
+	for i := range ans {
+		if len(ans[i]) > len(ans[index]) {
+			index = i
+		}
+	}
+	return ans[index]
+}
