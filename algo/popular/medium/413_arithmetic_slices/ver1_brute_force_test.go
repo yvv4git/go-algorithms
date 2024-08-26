6c849663f03af2cb77f4885ea2a2f6be7bf09dbd
yvv4git <yvv4m@mail.ru>
2024-08-26 14:58:12 +0300

    Algorithm - 413. Arithmetic Slices [Medium]

---
+++ algo/popular/medium/413_arithmetic_slices/ver1_brute_force_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func Test_countArithmeticSlices(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// Базовые случаи
+		{"Empty array", args{[]int{}}, 0},
+		{"Single element", args{[]int{1}}, 0},
+		{"Two elements", args{[]int{1, 2}}, 0},
+		{"Three elements, arithmetic slice", args{[]int{1, 2, 3}}, 1},
+		{"Three elements, no arithmetic slice", args{[]int{1, 3, 2}}, 0},
+
+		// Случаи с несколькими арифметическими срезами
+		{"Multiple arithmetic slices", args{[]int{1, 2, 3, 4, 5}}, 6},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countArithmeticSlices(tt.args.nums); got != tt.want {
+				t.Errorf("countArithmeticSlices() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ algo/popular/medium/413_arithmetic_slices/ver2_sliding_window_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func Test_numberOfArithmeticSlicesV2(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// Базовые случаи
+		{"Empty array", args{[]int{}}, 0},
+		{"Single element", args{[]int{1}}, 0},
+		{"Two elements", args{[]int{1, 2}}, 0},
+		{"Three elements, arithmetic slice", args{[]int{1, 2, 3}}, 1},
+		{"Three elements, no arithmetic slice", args{[]int{1, 3, 2}}, 0},
+
+		// Случаи с несколькими арифметическими срезами
+		{"Multiple arithmetic slices", args{[]int{1, 2, 3, 4, 5}}, 6},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfArithmeticSlicesV2(tt.args.nums); got != tt.want {
+				t.Errorf("numberOfArithmeticSlicesV2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
