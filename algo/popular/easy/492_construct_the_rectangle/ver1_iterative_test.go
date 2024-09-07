f7f887de93516cd2a97e31638a4b60f0d6c65368
yvv4git <yvv4m@mail.ru>
2024-09-07 22:59:06 +0300

    Algorithm - 492. Construct the Rectangle [Easy]

---
+++ algo/popular/easy/492_construct_the_rectangle/ver1_iterative_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_constructRectangle(t *testing.T) {
+	type args struct {
+		area int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "Case 1: Area is a prime number",
+			args: args{area: 37},
+			want: []int{37, 1},
+		},
+		{
+			name: "Case 2: Area is a perfect square",
+			args: args{area: 36},
+			want: []int{6, 6},
+		},
+		{
+			name: "Case 3: Area is a composite number",
+			args: args{area: 48},
+			want: []int{8, 6},
+		},
+		{
+			name: "Case 4: Area is 1",
+			args: args{area: 1},
+			want: []int{1, 1},
+		},
+		{
+			name: "Case 5: Area is a large number",
+			args: args{area: 1000000},
+			want: []int{1000, 1000},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := constructRectangle(tt.args.area); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("constructRectangle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
