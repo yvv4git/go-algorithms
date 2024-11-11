2522589e1f8c92497a298011b75a28fec7b4689b
yvv4git <yvv4m@mail.ru>
2024-11-11 22:18:49 +0300

    Algorithm - 94. Binary Tree Inorder Traversal [Easy]

---
+++ algo/popular/easy/94_binary_tree_inorder_traversal/ver1_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_inorderTraversal(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "CASE-1",
+			args: args{
+				root: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 1,
+					},
+					Right: &TreeNode{
+						Val: 3,
+					},
+				},
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "CASE-2",
+			args: args{
+				root: nil,
+			},
+			want: []int{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := inorderTraversal(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("inorderTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
